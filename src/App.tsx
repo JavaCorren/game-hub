eec36a0b0c3dabd716e44898a8f352f2d51e369c
Gorian Zhang <Guorui.Zhang@nzqa.govt.nz>
2024-12-18 20:36:59 +1300

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <>OK</>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main" `,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
