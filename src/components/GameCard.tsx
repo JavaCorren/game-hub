7a6f129c4c3dfa95d82ab8b1a1c88f6e71ab7c23
Gorian Zhang <Guorui.Zhang@nzqa.govt.nz>
2024-12-19 20:24:45 +1300

    display critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatFormIconList from "./PlatFormIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -10,7 +12,15 @@ const GameCard = ({ game }: Props) => {
     <Card borderRadius={10} overflow={"hidden"}>
       <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize={'2xl'}>{game.name}</Heading>
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <HStack justifyContent={'space-between'}>
+          <PlatFormIconList
+            platforms={game.parent_platforms.map(
+              (platform) => platform.platform
+            )}
+          ></PlatFormIconList>
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
