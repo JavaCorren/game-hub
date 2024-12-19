7211f7c089f297c1cd12326bde1c640eefc61ce9
Gorian Zhang <Guorui.Zhang@nzqa.govt.nz>
2024-12-19 19:29:21 +1300

    build game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,9 +3,10 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGamesResponse {
@@ -14,26 +15,24 @@ interface FetchGamesResponse {
 }
 
 const useGames = () => {
-
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
-    useEffect(() => {
-      
+  useEffect(() => {
     const controller = new AbortController();
 
     apiClient
-      .get<FetchGamesResponse>("/games", {signal: controller.signal})
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
-        .catch((err) => {
-            if (err instanceof CanceledError) return;
-            setError(err.message)
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
       });
-        
+
     return () => controller.abort();
   }, []);
 
-  return {games, error}
-}
+  return { games, error };
+};
 
 export default useGames
