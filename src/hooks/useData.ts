6020c0825c89a23b40bc0f9b0dd42b6f30ce1123
Gorian Zhang <Guorui.Zhang@nzqa.govt.nz>
2024-12-21 23:51:50 +1300

    highlight the selected genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -10,7 +10,7 @@ interface FetchResponse<T> {
 const useData = <T>(
   endpoint: string,
   requestConfig?: AxiosRequestConfig,
-  dependencies?: any[]
+  dependencies?: unknown[]
 ) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
