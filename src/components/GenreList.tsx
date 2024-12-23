e747ca5f5f2fae1f6cdc5e138cb0f47b377891f8
Gorian Zhang <Guorui.Zhang@nzqa.govt.nz>
2024-12-23 18:03:37 +1300

    cleanup the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 
 import {
   Button,
+  Heading,
   HStack,
   Image,
   List,
@@ -10,7 +11,6 @@ import {
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
-
 interface Props {
   selectedGenre: Genre | null;
   onSelectGenre: (genre: Genre) => void;
@@ -19,9 +19,13 @@ interface Props {
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data: genres, isLoading } = useGenres();
 
+  if (isLoading) return <Spinner />;
+
   return (
     <>
-      {isLoading && <Spinner />}
+      <Heading fontSize={"2xl"} marginBottom={3}>
+        Genres
+      </Heading>
       <List>
         {genres.map((genre) => (
           <ListItem key={genre.id} paddingY={"5px"}>
@@ -29,9 +33,12 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
               <Image
                 boxSize={"32px"}
                 borderRadius={8}
+                objectFit={"cover"}
                 src={getCroppedImageUrl(genre.image_background)}
               />
               <Button
+                whiteSpace={"normal"}
+                textAlign={"left"}
                 fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 fontSize={"lg"}
