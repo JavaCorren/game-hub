c8c517d8fcb4eb7841c0fead8ae02d686a71662c
Gorian Zhang <Guorui.Zhang@nzqa.govt.nz>
2024-12-23 18:06:38 +1300

    cleanup the game

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,11 @@ const GameGrid = ({ gameQuery }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, xl: 3 }} spacing={3} paddingX={2}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={6}
+        paddingX={2}
+      >
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
